1bca742e21fef1aac821667508fcd4d6dac85c00
Sejoslaw <k.dobrzynski94@gmail.com>
2022-07-07 14:30:08 +0200

    !Preparation for idea #2;

--- src/Tools/Dova.Tools.JavaClassStructureGenerator/Program.cs
+++ src/Tools/Dova.Tools.JavaClassStructureGenerator/Program.cs
@@ -1,7 +1,1 @@
-﻿using Dova.Tools.JavaClassStructureGenerator.Generators;
-using Dova.Tools.JavaClassStructureGenerator.Models;
-
-var model = new JavaClassDetailsModel();
-var generator = new StructureGenerator(model);
-
-generator.Run();
+﻿Console.WriteLine("Hello World !");
