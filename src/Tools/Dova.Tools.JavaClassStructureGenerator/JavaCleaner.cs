d2f72b0c02a66cb940de48ba5f05ada057ba60e0
Sejoslaw <k.dobrzynski94@gmail.com>
2022-07-26 21:42:20 +0200

    !method name cleaning;

--- src/Tools/Dova.Tools.JavaClassStructureGenerator/JavaCleaner.cs
+++ src/Tools/Dova.Tools.JavaClassStructureGenerator/JavaCleaner.cs
@@ -2,8 +2,8 @@ namespace Dova.Tools.JavaClassStructureGenerator;
 
 internal static class JavaCleaner
 {
-    // TODO: Add support for methods with names like 'lambda$indent$1'
-    public static string CleanMethodName(string methodName) => methodName;
+    public static string CleanMethodName(string methodName) => 
+        methodName.Replace("$", "_");
 
     // TODO: Add support for types like 'java.lang.invoke.TypeDescriptor$OfField<java.lang.Class<? extends PrintStream>[]>'
     // TODO: Add support for types like 'java.lang.@ref.FinalReference<java.lang.Object>' (see C# @ref keyword wrapped)
