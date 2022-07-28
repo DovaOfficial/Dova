b3d6c35842c9e172605bf24b20d5affa02be673e
Sejoslaw <k.dobrzynski94@gmail.com>
2022-07-28 21:46:14 +0200

    !cleaning namespace;

--- src/Tools/Dova.Tools.JavaClassStructureGenerator/Builders/NamespaceBuilder.cs
+++ src/Tools/Dova.Tools.JavaClassStructureGenerator/Builders/NamespaceBuilder.cs
@@ -7,6 +7,10 @@ internal class NamespaceBuilder : AbstractBuilder
     public override IEnumerable<string> Build(ClassDefinitionModel model, int tabs = 0)
     {
         yield return AppendLine("");
-        yield return AppendLine($"namespace Dova.JDK.{model.ClassDetailsModel.PackageName};");
+        
+        var ns = $"Dova.JDK.{model.ClassDetailsModel.PackageName}";
+        var cleanedNamespace = JavaCleaner.CleanJavaClassName(ns);
+        
+        yield return AppendLine($"namespace {cleanedNamespace};");
     }
 }
