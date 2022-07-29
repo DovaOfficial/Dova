f840611d429f2b55aa512f0a5656ebe17bdaaec5
Sejoslaw <k.dobrzynski94@gmail.com>
2022-07-29 23:52:18 +0200

    +comment;

--- src/Tools/Dova.Tools.JavaClassStructureGenerator/Builders/InnerClassesBuilder.cs
+++ src/Tools/Dova.Tools.JavaClassStructureGenerator/Builders/InnerClassesBuilder.cs
@@ -5,6 +5,7 @@ namespace Dova.Tools.JavaClassStructureGenerator.Builders;
 // TODO: Rework inner classes where parent class has generic args
 // TODO: Try and exclude inner classes to different files where namespace is build with inner class like: java.util.Separator.OfInt => java.util.separator.OfInt
 // TODO: OR add it after the parent class declaration
+// TODO: OR add a namespace with capital letter so it won't need to be modified later on
 internal class InnerClassesBuilder : AbstractBuilder
 {
     private static ClassBuilder Builder { get; } = new();
