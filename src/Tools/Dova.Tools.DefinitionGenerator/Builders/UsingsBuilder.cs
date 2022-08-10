a3fb2d7a93ff24760e8ee36abb89b12a84856a4c
Sejoslaw <k.dobrzynski94@gmail.com>
2022-08-10 07:38:06 +0200

    !fixed usings;

--- src/Tools/Dova.Tools.DefinitionGenerator/Builders/UsingsBuilder.cs
+++ src/Tools/Dova.Tools.DefinitionGenerator/Builders/UsingsBuilder.cs
@@ -1,5 +1,6 @@
 using Dova.Common;
 using Dova.Common.InterfaceFactory;
+using Dova.Core;
 using Dova.Tools.DefinitionGenerator.Models;
 
 namespace Dova.Tools.DefinitionGenerator.Builders;
@@ -9,6 +10,7 @@ internal class UsingsBuilder : AbstractBuilder
     public override IEnumerable<string> Build(FileInfo outputFile, ClassDefinitionModel model, int tabs = 0)
     {
         yield return AppendLine("");
+        yield return AppendLine($"using { typeof(DovaVM).Namespace };");
         yield return AppendLine($"using { typeof(JavaObject).Namespace };");
         yield return AppendLine($"using { typeof(DovaInterfaceFactory).Namespace };");
         yield return AppendLine("");
